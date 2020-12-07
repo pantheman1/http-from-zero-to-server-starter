c5322bd88c2529462af8b648683ffda76734c34d
Peter Anderson <peterburtonanderson@gmail.com>
2020-12-07 15:34:20 -0700

    Phase 2 complete

--- server.js
+++ server.js
@@ -13,10 +13,31 @@ http.createServer(function(req, res) {
         console.log('Request has been successfully created');
         res.writeHead(201);
         res.end();
-    } else if ()
-
-
-    else {
+    } else if (req.url === '/Forbidden') {
+        console.log('Request is forbidden. Error 403.')
+        res.writeHead(403)
+        res.end();
+    } else if (req.url === '/Found') {
+        console.log('Request redirected. 302 Found.');
+        res.writeHead(302);
+        res.end();
+    } else if (req.url === '/Gateway-Timeout') {
+        console.log('Server error. 504 Gateway-Timeout');
+        res.writeHead(504);
+        res.end();
+    } else if (req.url === '/Internal-Server-Error') {
+        console.log('Server error. 500 Internal-Server-Error');
+        res.writeHead(500);
+        res.end();
+    } else if (req.url === '/Moved-Permanently') {
+        console.log('Request redirected. 301 Moved-Permanently');
+        res.writeHead(301);
+        res.end();
+    } else if (req.url === '/Unauthorized') {
+        console.log('Request unauthorized')
+        res.writeHead(401);
+        res.end();
+    } else {
         res.writeHead(404);
         console.log('Status code: 404');
         res.end();
