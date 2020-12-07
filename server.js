f27139eca42e103345019f7281c1d609013e014f
Alex Rodriguez <alex@email.com>
2020-12-07 18:15:09 -0400

    Second commit - pahse2

--- server.js
+++ server.js
@@ -5,7 +5,22 @@ http.createServer(function(req, res) {
         console.log('Inbound "OK" request being processed...')
         res.writeHead(200);
         res.end();
+    } else if (req.url === '/Bad-Request') {
+        console.log('This is a bad request. Error 400.');
+        res.writeHead(400);
+        res.end();
+    } else if (req.url === '/Created') {
+        console.log('Request has been successfully created');
+        res.writeHead(201);
+        res.end();
+    } else if ()
+
+
+    else {
+        res.writeHead(404);
+        console.log('Status code: 404');
+        res.end();
     }
 }).listen(3000, function() {
     console.log('Listening for request on port 3000...')
-})
+});
